96055801bbab86e39db67c4796d548b83c1668b8
unknown <15982887241@163.com>
2018-10-16 15:30:34 +0800

    first commit

---
+++ external_library/Scripts/activate.bat
@@ -0,0 +1,32 @@
+@echo off
+set "VIRTUAL_ENV=C:\20180920_pycharm\book_manage\external_library"
+
+if not defined PROMPT (
+    set "PROMPT=$P$G"
+)
+
+if defined _OLD_VIRTUAL_PROMPT (
+    set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
+)
+
+if defined _OLD_VIRTUAL_PYTHONHOME (
+    set "PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%"
+)
+
+set "_OLD_VIRTUAL_PROMPT=%PROMPT%"
+set "PROMPT=(external_library) %PROMPT%"
+
+if defined PYTHONHOME (
+    set "_OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%"
+    set PYTHONHOME=
+)
+
+if defined _OLD_VIRTUAL_PATH (
+    set "PATH=%_OLD_VIRTUAL_PATH%"
+) else (
+    set "_OLD_VIRTUAL_PATH=%PATH%"
+)
+
+set "PATH=%VIRTUAL_ENV%\Scripts;%PATH%"
+
+:END
